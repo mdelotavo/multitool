0ade3b292545da9a88d6d547811c0cbbafc3474b
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-05-01 19:04:44 +1000

    Do https://click.palletsprojects.com/en/7.x/quickstart/

--- multitool/__main__.py
+++ multitool/__main__.py
@@ -6,6 +6,8 @@ import requests
 
 import click
 
+from multitool import APP
+from multitool import __version__ as version
 from multitool.utils import show_message
 
 URL = 'https://en.wikipedia.org/wiki/"Hello,_World!"_program'
@@ -23,7 +25,27 @@ def hello(count, name):
     for _ in range(count):
         click.echo(f"Hello, {name}!")
 
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(version, '-V', '--version')
+def cli():
+    pass
+
+@cli.command()
+def initdb():
+    click.echo('Initialized the database')
+
+@cli.command()
+def dropdb():
+    click.echo('Dropped the database')
+
+def main():
+    cli(prog_name=APP)
+    cli.add_command(initdb)
+    cli.add_command(dropdb)
+
 
 if __name__ == '__main__':
     # do_hello() # pragma: no cover
-    hello() # pragma: no cover
+    # hello() # pragma: no cover
+    # cli() # pragma: no cover
+    main() # pragma: no cover
