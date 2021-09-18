450dd6ee9d30b9f9930ba37f22b1e8a9f23dea55
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2021-09-18 21:46:20 +1000

    Fix toggles

--- multitool/__init__.py
+++ multitool/__init__.py
@@ -3,6 +3,7 @@ __version__ = '0.3.0'
 description = 'General-purpose command-line interface with plugins support'
 # long_description = 'General-purpose command-line interface with plugins support'
 
+import builtins
 from pathlib import Path
 
 from multitool.utils import generate_path_str
@@ -17,3 +18,6 @@ MULTITOOL_LOG_FILE = generate_path_str(MULTITOOL_DIRECTORY, f'{APP}.log')
 MULTITOOL_PLUGINS_DIRECTORY = generate_path_str(MULTITOOL_DIRECTORY, 'plugins')
 MULTITOOL_PLUGINS_CONFIG_FILE = generate_path_str(MULTITOOL_PLUGINS_DIRECTORY, 'config')
 MULTITOOL_PLUGINS_PATH = generate_path_str(MULTITOOL_PLUGINS_DIRECTORY, '__init__.py')
+
+builtins.MULTITOOL_TOGGLE_SILENT = MULTITOOL_TOGGLE_SILENT
+builtins.MULTITOOL_TOGGLE_VERBOSE = MULTITOOL_TOGGLE_VERBOSE
