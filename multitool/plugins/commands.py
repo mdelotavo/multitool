9fc5c758a4a6ce498c34990600716f88ba9a97ff
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2026-08-01 22:47:05 +1000

    update

--- multitool/plugins/commands.py
+++ multitool/plugins/commands.py
@@ -39,6 +39,25 @@ except ImportError:
     HAS_GIT = False
     HELP = "Manage plugin repositories. Git is required."
 
+PROJECT_STRUCTURE = {
+  "pyproject.toml": None,
+  "{app}": {
+    "__init__.py": None,
+    "__main__.py": None,
+    "cls.py": None,
+    "console.py": None,
+    "exceptions.py": None,
+    "silent.py": None,
+    "utils.py": None,
+    "utils_init.py": None,
+    "verbose.py": None,
+    "submodule": {
+      "__init__.py": None,
+      "commands.py": None,
+    },
+  },
+}
+
 
 def require_git():
     if not HAS_GIT:
@@ -300,12 +319,23 @@ def print_commit(name):
     console.echo(repo.git.log("--pretty=format:%h - %s (%cr) <%an>", "-1"))
 
 
+def create_structure(root: Path, structure: dict, app: str):
+    for name, children in structure.items():
+        path = root / name.format(app=app)
+
+        if children is None:
+            touch(path)
+        else:
+            mkdir(path)
+            create_structure(path, children, app)
+
+
 @click.group(help=HELP)
 def plugins():
     pass
 
 
-@plugins.command()
+@plugins.command(help="Edit the plugin source configuration.")
 @common_silent_options
 @common_verbose_options
 @click.option("-a/-A", "--apply-changes/--no-apply-changes", default=False)
@@ -322,7 +352,7 @@ def configure(silent, verbose, apply_changes):
         console.echo(f"\nRun `{APP} plugins update` to apply changes.\n")
 
 
-@plugins.command()
+@plugins.command(help="Install, update, and synchronize plugins with the configured sources.")
 @common_silent_options
 @common_verbose_options
 @click.option("-n", "--name")
@@ -338,7 +368,7 @@ def update(silent, verbose, name):
     install_plugin_dependencies(name)
 
 
-@plugins.command()
+@plugins.command(help="Show configured plugins or plugin information.")
 @common_silent_options
 @common_verbose_options
 @click.option("-n", "--name")
@@ -399,7 +429,7 @@ def show(silent, verbose, name, show_commit_only, show_dependencies_only):
         console.echo(f"{k}: {v}")
 
 
-@plugins.command()
+@plugins.command(help="Remove unmanaged plugin repositories.")
 @common_silent_options
 @common_verbose_options
 def prune(silent, verbose):
@@ -407,7 +437,7 @@ def prune(silent, verbose):
     prune_repos()
 
 
-@plugins.command()
+@plugins.command(help="Create a new plugin project.")
 @common_silent_options
 @common_verbose_options
 @click.argument("name")
@@ -601,3 +631,22 @@ Next steps:
            {APP} plugins update
 """
     )
+
+
+@plugins.command(help="Create the directory structure for a new Python package project.")
+@click.argument("name")
+@click.argument(
+  "directory",
+  type=click.Path(path_type=Path),
+  default=".",
+  required=False,
+)
+@common_silent_options
+@common_verbose_options
+def bootstrap(silent, verbose, name, directory):
+    root = directory / name
+    mkdir(root)
+
+    create_structure(root, PROJECT_STRUCTURE, name)
+
+    console.echo(f"Created project: {root}")
